39e84d59d118b3b48eee552c6a8c6ba4abcb0260
mercybaffour <mercy.baffour@gmail.com>
2022-10-31 15:23:32 -0400

    added script to call API

---
+++ public/script.js
@@ -0,0 +1,20 @@
+
+
+// //GET API REQUEST
+// async function getVisits() {
+//     try {
+//         let response = await fetch('https://5exwtt2sz1.execute-api.us-east-1.amazonaws.com/Prod/count');
+//         let data = await response.json();
+//         console.log(data);
+//         document.querySelector("#visits").innerHTML = data['visitorCount'];
+//         return data;
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
+
+// getVisits();
+
+
+
+
