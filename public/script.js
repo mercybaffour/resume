071a7263589f0bd0cb2ac15dc1b14f0e0662aa78
Mercy Baffour <mercy.baffour@gmail.com>
2023-02-20 15:32:45 -0500

    changed styling of home and resume page

--- public/script.js
+++ public/script.js
@@ -1,4 +1,32 @@
+//Hamburger Menu Selection
+const openNavMenu = document.querySelector('.menu-bar button');
+const closeNavMenu = document.querySelector('.close-nav button');
+const navbar = document.querySelector('.navbar');
 
+//Slider Selection
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+//Adding responsive nav menu toggle functionality
+function toggleNavMenu(){
+    navbar.classList.toggle('show');
+}
+
+openNavMenu.addEventListener("click", toggleNavMenu);
+closeNavMenu.addEventListener("click", toggleNavMenu);
+
+//Adding Slider Functionality
+nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+});
 
 //GET API REQUEST
 async function getVisits() {
